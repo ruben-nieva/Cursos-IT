0ccc9df4659185cf1b006fbd302b7a7de83cdcfe
ruben-nieva <ruben.nieva@gmail.com>
2016-06-30 23:38:38 -0300

    Fixed to run as JAR - Needed for Docker.

--- src/main/java/demo/CursosItApplication.java
+++ src/main/java/demo/CursosItApplication.java
@@ -2,11 +2,26 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CursosItApplication {
-
+public class CursosItApplication extends SpringBootServletInitializer{
+	
+	/**
+	 * Used when run as JAR
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CursosItApplication.class, args);
+	}
+	
+	
+	/**
+	 * Used when run as a WAR 
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(CursosItApplication.class);
 	}
 }
--- src/main/java/demo/config/WebSecurityConfig.java
+++ src/main/java/demo/config/WebSecurityConfig.java
@@ -41,5 +41,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //           .antMatchers("/resources/**"); // #3
 //    }
     
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests().anyRequest().authenticated();
+//        http
+//                .formLogin().failureUrl("/login?error")
+//                .defaultSuccessUrl("/")
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+//                .permitAll();
+//    }
+    
     
 }
